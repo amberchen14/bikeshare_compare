5608a0d325c5a137cc99982209bc82a0c3d6be86
amber <amber14@utexas.edu>
2020-06-05 15:57:10 -0700

    test spark

--- dataset/read_from_s3.py
+++ dataset/read_from_s3.py
@@ -13,11 +13,13 @@ import json, os, sys, time
 import boto3
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.types import StringType, DateType
-from pyspark.sql.functions import udf
+from pyspark.sql.functions import udf, col
 import configparser
+from functools import reduce 
 
-
-#os.environ['PYSPARK_SUBMIT_ARGS']='--jars $SPARK_HOME/jars/aws-java-sdk-1.7.4.jar,$SPARK_HOME/jars/hadoop-aws-2.7.7.jar,$SPARK_HOME/jars/jets3t-0.9.4.jar spark-shell'
+#s.environ['PYSPARK_SUBMIT_ARGS']='--jars /home/amber/spark/jars/aws-java-sdk-1.11.30.jar,/home/amber/spark/jars/hadoop-aws-2.7.7.jar,/home/amber/spark/jars/jets3t-0.9.4.jar pyspark-shell'i
+os.environ['PYSPARK_SUBMIT_ARGS']='--jars spark/jars/aws-java-sdk-1.11.30.jar,spark/jars/hadoop-aws-2.7.7.jar,spark/jars/jets3t-0.9.4.jar pyspark-shell'
+os.environ['PYTHONHASHSEED']='0'
 config = configparser.ConfigParser()
 aws_profile='default'
 config.read(os.path.expanduser("~/.aws/credentials"))
@@ -31,15 +33,22 @@ access_region = config.get(aws_profile, "region")
 spark = SparkSession \
 .builder \
 .appName("Ingesting raw json files into Spark DF for processing") \
-.config('spark.executor.memory', '2g') \
+.config("spark.cleaner.referenceTracking", "false")\
+.config("spark.cleaner.referenceTracking.blocking", "false")\
+.config("spark.cleaner.referenceTracking.blocking.shuffle", "false")\
+.config("spark.cleaner.referenceTracking.cleanCheckpoints", "false")\
+.config('spark.executor.memory', '6g') \
 .config('spark.executor.cores', '2') \
-.config('spark.driver.cores','2') \
-.config('spark.default.parallelism', '10') \
+.config('spark.default.parallelism', '100') \
 .getOrCreate()
+#.config("spark.master", "spark://ip-10-0-0-13.us-west-2.compute.internal:7077")\
+#.config('spark.driver.cores','2') \
+#.config('spark.driver.memory', '3g') \
+
+
 
 # Create Spark Context
 sc = spark.sparkContext
-
 
 # This sets the config with appropriate security credentials for s3 access
 hadoop_conf=sc._jsc.hadoopConfiguration()
@@ -52,11 +61,16 @@ hadoop_conf.set("com.amazonaws.services.s3a.enableV4", "true")
 # This is the S3 URL that I want to read json file(s) directly from
 # Notably, "s3a" is a way to get very large files.
 # "s3n" is an older version of this that can also get large files but not as large (up to a few GBs)
+df=None
 s3_url = "s3://de-club-2020/citibike/"
 s3_dwd =  "s3a://de-club-2020/citibike/"
 
-fnames=os.popen('aws s3 ls '+s3_url+" | awk '{print $4}'")
-df=None
+s3_url = "s3://de-club-2020/bluebike/"
+s3_dwd =  "s3a://de-club-2020/bluebike/"
+
+fnames=os.popen('aws s3 ls '+s3_url+" | awk '{print $4}'").readlines()
+
+#num_table = 0
 for f in fnames:
     if '.zip' in f or '.csv' not in f :
         continue
@@ -64,10 +78,57 @@ for f in fnames:
     print(f)
     if df is None:
         df=spark.read.load(f, format='csv', header='true')
+        df=df.orderBy(df['starttime'].desc())
     else:
         right=spark.read.load(f, format='csv', header='true')
+        right=right.orderBy(right['starttime'].desc())
         df=df.union(right)
+    #change column name
+    df = reduce(lambda df, idx: df.withColumnRenamed(df.columns[idx], df.columns[idx].replace(" ", "_")), range(len(df.columns)), df)
+    
+   # num_table +=1
+    #if num_table ==10:
+    #    break
 
+df_citibike
+df_bluebike
+
+#df_citibike.cache()
+df_citibike.createOrReplaceTempView("df_citibike")
+spark.sql('CACHE LAZY TABLE df_citibike')
+citi_start_station=df_citibike.select(['start_station_id', 
+                                  'start_station_name',
+                                  'start_station_latitude',
+                                  'start_station_longitude']).distinct()
+
+citi_start_station.createOrReplaceTempView("citi_start_station")
+spark.sql('CACHE LAZY TABLE citi_start_station')
+
+
+
+
+citi_station=df_citibike.groupby(['start_station_id', 
+                                  'start_station_name',
+                                  'start_station_latitude',
+                                  'start_station_longitude'])
+
+df_sub=df_citibike.groupby(['start_station_id', 'start_station_name']).count()
+df_sub.groupBy("start_station_id").count().filter("'count'>1").sort(col('count').desc()).show()
+
+DataFrame=df_sub.groupby(['start_station_id']).count().filter("`count` >= 1").sort(col('count').desc())
+
+
+  df.filter((df.d<5)&((df.col1 != df.col3) |
+                    (df.col2 != df.col4) & 
+                    (df.col1 ==df.col3)))\
+    .show()
+    
+#spark.sql("CACHE TABLE df_citibike")
+spark.sql("create table df_sub USING HIVE select distinct start_station_id, \
+          start_station_name, \
+          count(*) from df_citibike \
+          group by start_station_id, start_station_name\
+          order by start_station_id")
 
 
 # Now I can bring in my JSON file directly from s3 into Spark DF
@@ -77,3 +138,4 @@ df.printSchema()
 df.show()
 df.count()
 df.orderBy(df['end station id'].desc()).show()
+sc.stop()
