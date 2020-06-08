8c20e90708d9a681df1291819b47c428fa6fd082
amber <amber14@utexas.edu>
2020-06-07 20:49:57 -0700

    add new bike

--- dataset/read_from_s3.py
+++ dataset/read_from_s3.py
@@ -24,6 +24,7 @@ import pyspark.sql.functions as func
 os.environ['PYSPARK_SUBMIT_ARGS']='--jars spark/jars/aws-java-sdk-1.11.30.jar,spark/jars/hadoop-aws-2.7.7.jar,spark/jars/jets3t-0.9.4.jar pyspark-shell'
 os.environ['PYTHONHASHSEED']='0'
 
+s3_bucket='de-club-2020'
 #Get AWS key
 config = configparser.ConfigParser()
 aws_profile='default'
@@ -79,60 +80,75 @@ hadoop_conf.set("com.amazonaws.services.s3a.enableV4", "true")
 # This is the S3 URL that I want to read json file(s) directly from
 # Notably, "s3a" is a way to get very large files.
 # "s3n" is an older version of this that can also get large files but not as large (up to a few GBs)
-trip_df=None
-s3_url = "s3://de-club-2020/citibike/"
-s3_dwd =  "s3a://de-club-2020/citibike/"
 
-s3_url = "s3://de-club-2020/bluebike/"
-s3_dwd =  "s3a://de-club-2020/bluebike/"
+trip_db=None
+station_db=None
 
-fnames=os.popen('aws s3 ls '+s3_url+" | awk '{print $4}'").readlines()
+data = json.loads(bikeshare)
+for file in data:
+	s3_url="s3://"+s3_bucket+"/"+file['company']+"/"
+	s3_dwd="s3a://"+s3_bucket+"/"+file['company']+"/"
+	fnames=os.popen('aws s3 ls '+s3_url+" | awk '{print $4}'").readlines()
+	trip_df=None
+	station_df=None
+	for f in fnames:
+		if '.zip' in f or '.csv' not in f :
+			continue
+		url=s3_dwd+f.replace("\n", "")
+		#Trip file
+		if file['trip_file']['keyword'] in f:
+			if trip_df is None:
+				trip_df=spark.read.load(url, format='csv', header='true')
+				break
+			else:
+				right=spark.read.load(url, format='csv', header='true')
+				trip_df=trip_df.union(right)
+		#Station file
+		if file['station_file']['keyword'] in f:
+			if trip_df is None:
+				station_df=spark.read.load(url, format='csv', header='true')
+				#query = "select "+ 
+			else:
+				right=spark.read.load(url, format='csv', header='true')
+				trip_df=trip_df.union(right)
 
-#num_table = 0
-for f in fnames:
-    if '.zip' in f or '.csv' not in f :
-        continue
-    f=s3_dwd+f.replace("\n", "")
-    print(f)
-    if trip_df is None:
-        trip_df=spark.read.load(f, format='csv', header='true')
-        trip_df=trip_df.orderBy(trip_df['starttime'].desc())
-    else:
-        right=spark.read.load(f, format='csv', header='true')
-        right=right.orderBy(right['starttime'].desc())
-        trip_df=trip_df.union(right)
-    #change column name
-    trip_df = reduce(lambda trip_df, idx: trip_df.withColumnRenamed(trip_df.columns[idx], trip_df.columns[idx].replace(" ", "_")), range(len(trip_df.columns)), trip_df)
-    
- 
-station_df=trip_df.select(func.col("start_station_id").alias("id"), 
-		func.col("start_station_name").alias("name"),
-		func.col("start_station_latitude").alias("lat"),
-		func.col("start_station_longitude").alias("lon")
-		).distinct()
+	#replace column name from " " to "_"
+	trip_df = reduce(lambda trip_df, idx: trip_df.withColumnRenamed(trip_df.columns[idx], trip_df.columns[idx].replace(" ", "_")), range(len(trip_df.columns)), trip_df)
+	#No station file
+	if file['station_file']=='None':
+		station_df=trip_df.select(func.col(file["trip_file"]["start_station_id"]).alias("id"), 
+			func.col(file["trip_file"]["start_station_name"]).alias("name"),
+			func.col(file["trip_file"]["start_station_lat"]).alias("lat"),
+			func.col(file["trip_file"]["start_station_lon"]).alias("lon")
+			).distinct()
+		end_df=trip_df.select(func.col(file["trip_file"]["end_station_id"]).alias("id"), 
+			func.col(file["trip_file"]["end_station_name"]).alias("name"),
+			func.col(file["trip_file"]["end_station_lat"]).alias("lat"),
+			func.col(file["trip_file"]["end_station_lon"]).alias("lon")
+			).distinct()
+		station_df=station_df.union(end).distinct()
+		#station_rdd=station_df.rdd
+	
+	trip_df=trip_df.select(func.from_unixtime(func.unix_timestamp(file['trip_file']['start_time'], "yyyy-MM-dd HH:mm:ss")), #.alias('starttime') 
+		func.from_unixtime(func.unix_timestamp(file['trip_file']["end_time"], "yyyy-MM-dd HH:mm:ss")),  #.alias('endtime') 
+		file['trip_file']['start_station_id'],file['trip_file']['end_station_id'])
 
-end=trip_df.select(func.col("end_station_id").alias("id"), 
-		func.col("end_station_name").alias("name"),
-		func.col("end_station_latitude").alias("lat"),
-		func.col("end_station_longitude").alias("lon")
-		).distinct()
-station_df=station_df.union(end).distinct()
-station_rdd=station_df.rdd
+	trip_df.createOrReplaceTempView('trip_df')
+	query="select " + file['trip_file']['start_time'] + " as start_time, "+\
+		file['trip_file']['end_time'] + " as end_time, " +\
+		file['trip_file']['start_station_id'] + "as start_station_id, " +\
+		file['trip_file']['end_station_id'] + "as end_station_id " + \
+		"from trip_df" 
+		trip_df=spark.sql(query)
+		station_bike_usage= trip_df.select(func.col('start_station_id').alias('station_id'), func.col('starttime').alias('time'), func.lit(1).alias('action'))\
+			.union(trip_df.select(func.col('end_station_id').alias('station_id'), func.col('endtime').alias('time'), func.lit(-1).alias('action')))
+		window = Window.partitionBy("station_id").orderBy("time")  
+		station_bike_usage=station_bike_usage.select('station_id', 'time', 'action', func.sum('action').over(window).alias('usage'))
 
-
-trip_df=trip_df.select(func.from_unixtime(func.unix_timestamp('starttime', "yyyy-MM-dd HH:mm:ss")).alias('starttime'), 
-				func.from_unixtime(func.unix_timestamp("stoptime", "yyyy-MM-dd HH:mm:ss")).alias('endtime'), 
-				'start_station_id','end_station_id')
-
-
-trip_rdd=trip_df.rdd
 
 
 #rentBike= trip_df.select(func.col('start_station_id').alias('station_id'), func.col('starttime').alias('time')).withColumn('action', lit(0))
-station_bike_usage= trip_df.select(func.col('start_station_id').alias('station_id'), func.col('starttime').alias('time'), func.lit(1).alias('action'))\
-		.union(trip_df.select(func.col('end_station_id').alias('station_id'), func.col('endtime').alias('time'), func.lit(-1).alias('action')))
-window = Window.partitionBy("station_id").orderBy("time")  
-station_bike_usage=station_bike_usage.select('station_id', 'time', 'action', func.sum('action').over(window).alias('usage'))
+
 
 
 
