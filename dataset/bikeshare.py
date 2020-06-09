7f630428c60fe9e25c67c6c370e3a384206fe189
amber <amber14@utexas.edu>
2020-06-09 15:18:36 -0700

    add more cities

--- dataset/bikeshare.py
+++ dataset/bikeshare.py
@@ -1,4 +1,7 @@
 import json
+
+
+
 citibike={"company": "citibike",
           #  "state": "NY",
             "city": "NYC",
@@ -19,6 +22,109 @@ citibike={"company": "citibike",
 		},
 		"station_file": "None"  
 }
+
+
+divvy={"company": "divvy",
+           # "state": "MA",
+            "city": "Boston",
+            "trip_file":{
+                "keyword": "trip",
+                "version":[{
+                    "start_time":"start_time",
+                    "end_time": "end_time",
+                    "start_station_id": "from_station_id",
+                    "start_station_name":"from_station_name",
+                    "end_station_id": "to_station_id",
+                    "end_station_name":"to_station_name"
+                }
+                ]
+            },
+            "station_file": {
+                "keyword": "station",
+                "version":[{
+                    "id": "id",
+                    "name": "Name",
+                    "lon":"longitude",
+                    "lat":"latitude"                    
+                    }
+              ]
+
+            }
+}
+start_time  end_time    start_station_id    start_station_name  start_station_latitude  start_station_longitude end_station_id  end_station_name    end_station_latitude    end_station_longitude
+
+lyft={
+    "company": "lyft",
+            "trip_file":{
+                "keyword": "trip",
+                "version":[{
+                    "start_time":"start_time",
+                    "end_time": "end_time",
+                    "start_station_id": "start_station_id",
+                    "start_station_name":"start_station_name",
+                    "end_station_id": "start_station_id",
+                    "end_station_name":"start_station_name",
+                    "end_station_lon": "end_station_longitude",
+                    "end_station_lat":"end_station_latitude",
+                    "start_station_lon": "start_station_longitude",
+                    "start_station_lat":"start_station_latitude"                                        
+                }
+                ]
+            },
+
+
+}
+
+
+cogo={"company": "cogo",
+           # "state": "MA",
+            "city": "Boston",
+            "trip_file":{
+                "keyword": "trip",
+                "version":[{
+                    "start_time":"start_time",
+                    "end_time": "end_time",
+                    "start_station_id": "from_station_id",
+                    "start_station_name":"from_station_location",
+                    "end_station_id": "to_station_id",
+                    "end_station_name":"to_station_location"
+                }]
+            },
+            "station_file": {
+                "keyword": "station",
+                "version":[{
+                    "id": "Number",
+                    "name": "Name",
+                    "lon":"Longitude",
+                    "lat":"Latitude"                    
+                    },{
+                    "id": "Station_ID",
+                    "name": "Station",
+                    "lon":"Longitude",
+                    "lat":"Latitude"                        
+                }
+              ]
+
+            }
+}
+
+
+capital={"company": "capital",
+            "city": "DC",
+            "trip_file":{
+                "keyword": "trip",
+                "start_time":"Start_Date",
+                "end_time": "End_Date",
+                "start_station_id": "Start_station_number",
+                "start_station_name":"Start_station",
+                "end_station_id": "End_station_number",
+                "end_station_name":"End_station",
+            },
+         	"station_file": "None"  
+}
+
+
+
 bluebike={"company": "bluebike",
            # "state": "MA",
             "city": "Boston",
@@ -71,97 +177,8 @@ bluebike={"company": "bluebike",
 
             }
 }
-
-
-divvy={"company": "divvy",
-           # "state": "MA",
-            "city": "Boston",
-            "trip_file":{
-                "keyword": "trip",
-                "version":[{
-                    "start_time":"start_time",
-                    "end_time": "end_time",
-                    "start_station_id": "from_station_id",
-                    "start_station_name":"from_station_name",
-                    "end_station_id": "to_station_id",
-                    "end_station_name":"to_station_name"
-                },{
-                    "start_time":"starttime",
-                    "end_time": "stoptime",
-                    "start_station_id": "start_station_id",
-                    "start_station_name":"start_station_name",
-                    "start_station_lon": "start_station_longitude",
-                    "start_station_lat":"start_station_latitude",
-                    "end_station_id": "end_station_id",
-                    "end_station_name":"end_station_name",
-                    "end_station_lon": "end_station_longitude",
-                    "end_station_lat":"end_station_latitude"
-                },
-                ]
-            },
-            "station_file": {
-                "keyword": "station",
-                "version":[{
-                    "id": "Number",
-                    "name": "Name",
-                    "lon":"Longitude",
-                    "lat":"Latitude"                    
-                    },{
-                    "id": "Station_ID",
-                    "name": "Station",
-                    "lon":"Longitude",
-                    "lat":"Latitude"                        
-                }
-              ]
-
-            }
-}
-start_time  end_time    start_station_id    start_station_name  start_station_latitude  start_station_longitude end_station_id  end_station_name    end_station_latitude    end_station_longitude
-
-lyft={
-    "company": "lyft",
-            "trip_file":{
-                "keyword": "trip",
-                "version":[{
-                    "start_time":"start_time",
-                    "end_time": "end_time",
-                    "start_station_id": "start_station_id",
-                    "start_station_name":"start_station_name",
-                    "end_station_id": "start_station_id",
-                    "end_station_name":"start_station_name",
-                    "end_station_lon": "end_station_longitude",
-                    "end_station_lat":"end_station_latitude",
-                    "start_station_lon": "start_station_longitude",
-                    "start_station_lat":"start_station_latitude"                                        
-                }
-                ]
-            },
-
-
-}
-cogo={
-    start_time  end_time    bikeid  tripduration    from_station_location   from_station_id from_station_name   to_station_location to_station_id
-}
 bikeshare=json.dumps([bluebike])
-
-
-
-
-capital={"company": "capital",
-            "city": "DC",
-            "trip_file":{
-                "keyword": "trip",
-                "start_time":"Start_Date",
-                "end_time": "End_Date",
-                "start_station_id": "Start_station_number",
-                "start_station_name":"Start_station",
-                "end_station_id": "End_station_number",
-                "end_station_name":"End_station",
-            },
-         	"station_file": "None"  
-}
-
 
 bikeshare=json.dumps([bluebike, citibike])
                 
-bikeshare=json.dumps([bluebike])
+
