8ffb3b6e9c1b4bfcc06841dfbfbfbab4714ec18a
amber <amber14@utexas.edu>
2020-06-09 22:23:50 -0700

    slightly change

--- dataset/bikeshare.py
+++ dataset/bikeshare.py
@@ -1,6 +1,55 @@
 import json
 
 
+bluebike={"company": "bluebike",
+           # "state": "MA",
+            "city": "Boston",
+            "trip_file":{
+            	"keyword": "trip",
+            	"version":[{
+	                "start_time":"Start_Date",
+	                "end_time": "End_Date",
+	                "start_station_id": "Start_station_number",
+	                "start_station_name":"Start_station_name",
+	                "end_station_id": "End_station_number",
+	                "end_station_name":"End_station_name",
+	                "end_station_lon": "End_station_longitude",
+	                "end_station_lat":"End_station_latitude",	                
+	                "start_station_lon": "Start_station_longitude",
+	                "start_station_lat":"Start_station_latitude"
+            	},{
+	                "start_time":"starttime",
+	                "end_time": "stoptime",
+	                "start_station_id": "start_station_id",
+	                "start_station_name":"start_station_name",
+	                "start_station_lon": "start_station_longitude",
+	                "start_station_lat":"start_station_latitude",
+	                "end_station_id": "end_station_id",
+	                "end_station_name":"end_station_name",
+	                "end_station_lon": "end_station_longitude",
+	                "end_station_lat":"end_station_latitude"
+            	},
+            	]
+            },
+            "station_file": {
+                "keyword": "station",
+                "version":[{
+	                "id": "Number",
+	                "name": "Name",
+	                "lon":"Longitude",
+	                "lat":"Latitude"                 	
+	                },{
+		            "id": "Station_ID",
+		            "name": "Station",
+		            "lon":"Longitude",
+		            "lat":"Latitude"                        
+	            }
+              ]
+
+            }
+}
+bikeshare=json.dumps([bluebike])
+
 
 citibike={"company": "citibike",
           #  "state": "NY",
@@ -125,59 +174,6 @@ capital={"company": "capital",
 
 
 
-bluebike={"company": "bluebike",
-           # "state": "MA",
-            "city": "Boston",
-            "trip_file":{
-            	"keyword": "trip",
-            	"version":[{
-	                "start_time":"Start_Date",
-	                "end_time": "End_Date",
-	                "start_station_id": "Start_station_number",
-	                "start_station_name":"Start_station_name",
-	                "end_station_id": "End_station_number",
-	                "end_station_name":"End_station_name",
-	                "end_station_lon": "End_station_longitude",
-	                "end_station_lat":"End_station_latitude",	                
-	                "start_station_lon": "Start_station_longitude",
-	                "start_station_lat":"Start_station_latitude"
-            	},{
-	                "start_time":"starttime",
-	                "end_time": "stoptime",
-	                "start_station_id": "start_station_id",
-	                "start_station_name":"start_station_name",
-	                "start_station_lon": "start_station_longitude",
-	                "start_station_lat":"start_station_latitude",
-	                "end_station_id": "end_station_id",
-	                "end_station_name":"end_station_name",
-	                "end_station_lon": "end_station_longitude",
-	                "end_station_lat":"end_station_latitude"
-            	},
-            	]
-            },
-            "station_file": {
-                "keyword": "station",
-          #      "file_name":[
-          #      	"current_bluebikes_stations.csv", 
-          #      	"Hubway_Stations_as_of_July_2017.csv",
-          #      	"Hubway_Stations_2011_2016.csv"
-          #      ],
-                "version":[{
-	                "id": "Number",
-	                "name": "Name",
-	                "lon":"Longitude",
-	                "lat":"Latitude"                 	
-	                },{
-		            "id": "Station_ID",
-		            "name": "Station",
-		            "lon":"Longitude",
-		            "lat":"Latitude"                        
-	            }
-              ]
-
-            }
-}
-bikeshare=json.dumps([bluebike])
 
 bikeshare=json.dumps([bluebike, citibike])
                 
