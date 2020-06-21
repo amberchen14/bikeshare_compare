2f0aa9aa7b47af9efa9b0f4c851d1b5dd1d60317
amber <amber14@utexas.edu>
2020-06-20 20:11:53 -0700

    remove identify schema from file

--- dataset/data_process.py
+++ dataset/data_process.py
@@ -5,7 +5,6 @@ Created on Wed Jun  3 20:39:15 2020
 
 @author: amberchen
 """
-
 import os, sys, time, boto3, requests, json, glob, shutil, zipfile, configparser
 import s3fs
 from bs4 import BeautifulSoup
@@ -25,27 +24,33 @@ from data_process_func import * #write_to_psql, get_value_from_psql, get_unique_
 from psql_func import * 
 
 s3_bucket='de-club-2020'
-def process ():
-	spark=create_spark_session(s3_bucket)				
-	url="s3://"+s3_bucket+"/"
+schema_name='company_schema.json'
+
+def read_company_from_s3(bucket_name):
+	url="s3://"+bucket_name+"/"
 	query="aws s3 ls "+ url+  " | awk '{print $2}' "
 	companies=os.popen(query).readlines()	
-	for company in companies:
-		company=company.replace("/\n","")
-		if 'lyft' in company:
-			continue
-		s3_url="s3://"+s3_bucket+"/"+company+"/"
-		s3_dwd="s3a://"+s3_bucket+"/"+company+"/"
-		query="aws s3 ls "+ s3_url+  "  | awk '{$1=$2=$3=\"\"; print $0}' | sed 's/^[ \t]*//'"
-		fnames=os.popen(query).readlines()				
+	return companies
+
+def read_fname_from_s3(bucket_name, company):
+	url="s3://"+bucket_name+"/"+company+"/"
+	query="aws s3 ls "+ url+  "  | awk '{$1=$2=$3=\"\"; print $0}' | sed 's/^[ \t]*//'"
+	fnames=os.popen(query).readlines()	
+	return url, fnames
+
+def process ():
+	spark=create_spark_session(s3_bucket)		
+	company_schema=read_schema_from_s3(s3_bucket, schema_name)	
+	for schema in company_schema:
+		company=schema['company']
+		trip_key=schema['trip_file']['keyword']
+		station_key=schema['station_file']['keyword']
+		url, fnames=read_fname_from_s3(s3_bucket, company)		
 		station_max_row=get_value_from_psql(spark, "max (uid)", "station").toPandas()['max'][0]
-		print('station max row {}'.format(station_max_row))	
 		if station_max_row is None:
 			station_max_row = 0	
 		count = 0
 		station_df, station_from_trip_df, trip_df=None, None, None
-		pre_columns, sub_schema = None, None
-		schema=initial_schema(company)	
 		for f in fnames:
 			if '.zip' in f:
 				continue
@@ -53,23 +58,20 @@ def process ():
 				continue	
 			count+=1
 			f=f.replace("\n", "")
-			url=s3_dwd+f.replace("\n", "")
-			sub = spark.read.load(url, format='csv', header='true')
+			dwd_url="s3a://"+s3_bucket+"/"+company+"/"+	f.replace("\n", "")	
+			sub = spark.read.load(dwd_url, format='csv', header='true')
 			sub = reduce(lambda sub, idx: sub.withColumnRenamed(sub.columns[idx], 
 																sub.columns[idx].replace(" ", "_").lower()),
 																range(len(sub.columns)), sub)		
-			csv=pd.read_csv(s3_url+f)
+			csv=pd.read_csv(dwd_url, header=0)
 			csv.columns = map(str.lower, csv.columns)
-			csv.columns=csv.columns.str.replace("\n", "").str.replace(" ","_")		
-			if pre_columns is None or len(csv.columns.difference(pre_columns))!=0:
-				key, sub_schema, schema=update_schema(csv, f,schema)
-				pre_columns=csv.columns 
-			print("count: {}\nfile: {}\n\n".format(count, f, sub))
-			if key=='trip':
+			csv.columns=csv.columns.str.replace("\n", "").str.replace(" ","_")
+			if trip_key in f:
+				sub_schema=search_matched_schema(str(csv.columns), schema['trip_file'])
 				station_from_trip_df=station_from_trip_table(sub_schema, csv, station_from_trip_df)
 				trip_df=clean_trip_table(sub_schema, sub, trip_df)
-				continue			
-			if key=='station':
+			elif station_key in f:
+				sub_schema=search_matched_schema(str(csv.columns), schema['station_file'])
 				station_df=union_station_table(sub_schema, csv, station_df)
 		print("station_df:{}, station_from_trip_df:{}".format(station_df, station_from_trip_df))
 		station_df, station_max_row=get_unique_station_table(station_df, station_from_trip_df, station_max_row)
