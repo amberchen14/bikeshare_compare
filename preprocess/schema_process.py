5b9a6338a4414a9000b5f49bbce2102b773194ba
amber <amber14@utexas.edu>
2020-06-22 08:48:41 -0700

    combine files

--- preprocess/schema_process.py
+++ preprocess/schema_process.py
@@ -9,13 +9,44 @@ Created on Wed Jun  3 20:39:15 2020
 import os, sys, time, boto3, requests, json, glob, shutil, zipfile, configparser
 import urllib.request
 import pandas as pd
+from bs4 import BeautifulSoup
 from schema_func import * 
+from downlad_func import *
+
+tmpfolder='tmp/'
+dataset=os.getenv("HOME")+'/datasets/'
+gbfs={'citibike':'https://s3.amazonaws.com/tripdata/',
+        'capital':'https://s3.amazonaws.com/capitalbikeshare-data/',
+        'niceride':'https://s3.amazonaws.com/niceride-data/',
+        'bluebike':'https://s3.amazonaws.com/hubway-data/',
+        'divvy':'https://divvy-tripdata.s3.amazonaws.com/',
+        'cogo':'https://s3.amazonaws.com/cogo-sys-data/'
+            }
+other={'metro':'https://bikeshare.metro.net/about/data/'}
+
 
 s3_bucket='de-club-2020'
 schema_name='company_schema.json'
 
 
-def process ():	
+def download():
+    #gbfs structure
+    for fname in gbfs:
+        url=gbfs[fname]
+        dwd=dataset+fname+'/'
+        create_folder(dwd)
+        get_gbfs(url, dwd)
+        mv_all(dwd)
+
+    #others
+    for fname in other:
+        url=other[fname]
+        dwd=dataset+fname+'/'
+        create_folder(dwd)
+        get_other(url, dwd)
+        mv_all(dwd)   
+
+def schema_normalize ():	
 	company_schemas=[]
 	companies=read_company_from_s3(s3_bucket)
 	for company in companies:
@@ -44,4 +75,5 @@ def process ():
 	write_schema_to_s3(s3_bucket, company_schemas)
 
 if __name__ == '__main__':
-    process()
+	download()
+    schema_normalize()
