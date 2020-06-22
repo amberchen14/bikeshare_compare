eb8dd230b26382a6ff24e631648ff782c8bb67a1
amber <amber14@utexas.edu>
2020-06-22 09:06:35 -0700

    test

--- data_process/data_process_func.py
+++ data_process/data_process_func.py
@@ -1,11 +1,6 @@
 import pyspark.sql.functions as func
 from pyspark.sql.types import *
 
-def search_matched_schema(columns, schema):
-	for sub in schema['version']:
-		if sub['columns']==columns:
-			print(sub)
-			return sub
 		
 def get_unique_station_table(station_from_trip_df, station_df, station_pre_row):
 	if station_df is None and station_from_trip_df is None:
