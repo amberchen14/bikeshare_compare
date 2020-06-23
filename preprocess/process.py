cfe4d5bd3f3e881cb3b7c7928305a3b1cc1fe01c
amber <amber14@utexas.edu>
2020-06-22 17:35:36 -0700

    sperate download and schema normalize to two parts

--- preprocess/process.py
+++ preprocess/process.py
@@ -13,6 +13,7 @@ from bs4 import BeautifulSoup
 from schema_func import * 
 from downlad_func import *
 
+
 tmpfolder='tmp/'
 dataset=os.getenv("HOME")+'/datasets/'
 gbfs={'citibike':'https://s3.amazonaws.com/tripdata/',
@@ -27,7 +28,8 @@ other={'metro':'https://bikeshare.metro.net/about/data/'}
 s3_bucket='de-club-2020'
 schema_name='company_schema.json'
 
-def download_and_upload_s3():
+
+def download():
     #Download trip files follow gbfs structure placed in s3. 
     for fname in gbfs:
         url=gbfs[fname]
@@ -43,42 +45,11 @@ def download_and_upload_s3():
         create_folder(dwd)
         get_other(url, dwd)
         mv_all(dwd)   
-    #Upload all files to s3
    
 
-def schema_normalize ():
-	'''
-	This function reads all files placed in s3 and 
-	let user define schemas when the columns the system read does not face before.
-	'''
-	company_schemas=[]
-	companies=read_company_from_s3(s3_bucket)
-	for company in companies:
-		company=company.replace("/\n","")	
-		pre_columns, sub_schema = None, None
-		url, fnames=read_fname_from_s3(s3_bucket, company)			
-		schema=initial_schema(company, fnames)	
-		for f in fnames:
-			if '.zip' in f:
-				continue
-			if "csv" not in f:
-				continue	
-			f=f.replace("\n", "")
-			print("file name: {}".format(f))
-			csv=pd.read_csv(url+f, nrows=2)
-			csv.columns = map(str.lower, csv.columns)
-			csv.columns=csv.columns.str.replace("\n", "").str.replace(" ","_")		
-			if pre_columns is None or len(csv.columns.difference(pre_columns))!=0:
-				key, sub_schema, schema=update_schema(csv, f, schema)
-				pre_columns=csv.columns 
-		try:
-			company_schemas.append(schema) 
-		except:
-			company_schemas=[schema]
-	company_schemas=json.dumps(company_schemas)
-	write_schema_to_s3(s3_bucket, company_schemas)
+
 
 if __name__ == '__main__':
-    download_and_upload_s3()
-    schema_normalize()
+    download()
+
 
