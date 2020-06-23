caf5fb2b841b3aaff7f831c2700c629be17ac555
amberchen14 <amber14@utexas.edu>
2020-06-22 17:11:17 -0700

    Update process.py

--- preprocess/process.py
+++ preprocess/process.py
@@ -28,9 +28,8 @@ other={'metro':'https://bikeshare.metro.net/about/data/'}
 s3_bucket='de-club-2020'
 schema_name='company_schema.json'
 
-
-def download():
-    #gbfs structure
+def download_and_upload_s3():
+    #Download trip files follow gbfs structure placed in s3. 
     for fname in gbfs:
         url=gbfs[fname]
         dwd=dataset+fname+'/'
@@ -38,15 +37,21 @@ def download():
         get_gbfs(url, dwd)
         mv_all(dwd)
 
-    #others
+    #Download trip files placed in web (wget...)
     for fname in other:
         url=other[fname]
         dwd=dataset+fname+'/'
         create_folder(dwd)
         get_other(url, dwd)
         mv_all(dwd)   
+    #Upload all files to s3
+   
 
-def schema_normalize ():	
+def schema_normalize ():
+	'''
+	This function reads all files placed in s3 and 
+	let user define schemas when the columns the system read does not face before.
+	'''
 	company_schemas=[]
 	companies=read_company_from_s3(s3_bucket)
 	for company in companies:
@@ -75,5 +80,5 @@ def schema_normalize ():
 	write_schema_to_s3(s3_bucket, company_schemas)
 
 if __name__ == '__main__':
-	download()
+    download_and_upload_s3()
     schema_normalize()
